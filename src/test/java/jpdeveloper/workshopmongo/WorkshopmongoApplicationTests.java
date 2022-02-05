efcbdc8a7c3f8f5324908abb404bc7bea215b498
Joao Paulo <joaopaulodantasbernardo@gmail.com>
2022-02-04 21:28:35 -0300

    Projeto criado

---
+++ src/test/java/jpdeveloper/workshopmongo/WorkshopmongoApplicationTests.java
@@ -0,0 +1,13 @@
+package jpdeveloper.workshopmongo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkshopmongoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
